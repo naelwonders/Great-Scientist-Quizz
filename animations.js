b8461de1b5028e4bae3246b8f08647b18229b3bf
Nael Lammens <nslammens@gmail.com>
2023-09-11 16:50:48 +0200

    messages deleted

--- animations.js
+++
@@ -1,7 +0,0 @@
-function fadeIn(element) {
-    // Fade in animation
-}
-
-function slideTransition(element) {
-    // Slide transition animation
-}
--- gameLogic.js
+++
@@ -1,8 +0,0 @@
-
-function checkAnswer(selectedAnswer) {
-    // Check if the answer is correct: score
-}
-
-function nextQuestion() {
-    // Transition to the next question or end the game if no questions left
-}
